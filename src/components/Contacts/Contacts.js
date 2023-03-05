c40ca580d629c2f011a1fdc0e1225223a0940931
Roman <klymenkoromca18@gmail.com>
2023-03-05 12:42:00 +0200

    дз

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -9,14 +9,23 @@ import css from './Contacts.module.css';
 
 class Contacts extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('my-contacts'));
+    if (contacts && contacts.length) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     if (this.isDublicate(name)) {
